613986817ec769776a6315e39259f1bc0c8e865e
DaAlpha <daalpha@nologam.com>
2015-12-21 19:11:00 +0100

    v2.0 release

---
+++ client/dtc.lua
@@ -0,0 +1,16 @@
+-- Created by DaAlpha
+class 'DTC'
+
+function DTC:__init()
+	Events:Subscribe("LocalPlayerChat", self, self.LocalPlayerChat)
+end
+
+function DTC:LocalPlayerChat(args)
+	if args.text:lower() == "/dtc" then
+		Chat:Print("Distance to map center: ", Color.White, string.format("%.2f km",
+			LocalPlayer:GetPosition():Distance2D(Vector3.Zero) / 1000), Color.RoyalBlue)
+		return false
+	end
+end
+
+local dtc = DTC()
