53a15a0b5506f8a17dabb6fa9db8b11ab04f99e4
DaAlpha <daalpha@nologam.com>
2016-10-23 02:38:46 +0200

    Changed indentations

--- client/dtc.lua
+++ client/dtc.lua
@@ -2,15 +2,15 @@
 class 'DTC'
 
 function DTC:__init()
-	Events:Subscribe("LocalPlayerChat", self, self.LocalPlayerChat)
+  Events:Subscribe("LocalPlayerChat", self, self.LocalPlayerChat)
 end
 
 function DTC:LocalPlayerChat(args)
-	if args.text:lower() == "/dtc" then
-		Chat:Print("Distance to map center: ", Color.White, string.format("%.2f km",
-			LocalPlayer:GetPosition():Distance2D(Vector3.Zero) / 1000), Color.RoyalBlue)
-		return false
-	end
+  if args.text:lower() == "/dtc" then
+    Chat:Print("Distance to map center: ", Color.White, string.format("%.2f km",
+      LocalPlayer:GetPosition():Distance2D(Vector3.Zero) / 1000), Color.RoyalBlue)
+    return false
+  end
 end
 
 local dtc = DTC()
